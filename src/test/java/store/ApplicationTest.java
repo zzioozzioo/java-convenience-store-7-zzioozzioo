b5b2ea10b30a793162a40d8584f63c84b60a4035
zzioozzioo <zzioo1006@naver.com>
2024-11-11 00:58:20 +0900

    style: 코드 포맷팅

--- src/test/java/store/ApplicationTest.java
+++ src/test/java/store/ApplicationTest.java
@@ -1,13 +1,12 @@
 package store;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
-import java.time.LocalDate;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertNowTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -15,24 +14,24 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() -> {
             run("[물-1]", "N", "N");
             assertThat(output()).contains(
-                "- 콜라 1,000원 10개 탄산2+1",
-                "- 콜라 1,000원 10개",
-                "- 사이다 1,000원 8개 탄산2+1",
-                "- 사이다 1,000원 7개",
-                "- 오렌지주스 1,800원 9개 MD추천상품",
-                "- 오렌지주스 1,800원 재고 없음",
-                "- 탄산수 1,200원 5개 탄산2+1",
-                "- 탄산수 1,200원 재고 없음",
-                "- 물 500원 10개",
-                "- 비타민워터 1,500원 6개",
-                "- 감자칩 1,500원 5개 반짝할인",
-                "- 감자칩 1,500원 5개",
-                "- 초코바 1,200원 5개 MD추천상품",
-                "- 초코바 1,200원 5개",
-                "- 에너지바 2,000원 5개",
-                "- 정식도시락 6,400원 8개",
-                "- 컵라면 1,700원 1개 MD추천상품",
-                "- 컵라면 1,700원 10개"
+                    "- 콜라 1,000원 10개 탄산2+1",
+                    "- 콜라 1,000원 10개",
+                    "- 사이다 1,000원 8개 탄산2+1",
+                    "- 사이다 1,000원 7개",
+                    "- 오렌지주스 1,800원 9개 MD추천상품",
+                    "- 오렌지주스 1,800원 재고 없음",
+                    "- 탄산수 1,200원 5개 탄산2+1",
+                    "- 탄산수 1,200원 재고 없음",
+                    "- 물 500원 10개",
+                    "- 비타민워터 1,500원 6개",
+                    "- 감자칩 1,500원 5개 반짝할인",
+                    "- 감자칩 1,500원 5개",
+                    "- 초코바 1,200원 5개 MD추천상품",
+                    "- 초코바 1,200원 5개",
+                    "- 에너지바 2,000원 5개",
+                    "- 정식도시락 6,400원 8개",
+                    "- 컵라면 1,700원 1개 MD추천상품",
+                    "- 컵라면 1,700원 10개"
             );
         });
     }
