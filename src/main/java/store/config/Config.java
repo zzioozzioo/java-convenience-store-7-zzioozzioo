fe8cdc5d037d5a1abbb88fb73a0f1944cc6a889d
zzioozzioo <zzioo1006@naver.com>
2024-11-10 18:44:05 +0900

    refactor: ServiceConfig 제거
    - 컨트롤러에서 직접 StoreService 객체를 생성하도록 수정

--- src/main/java/store/config/Config.java
+++ src/main/java/store/config/Config.java
@@ -2,8 +2,6 @@ package store.config;
 
 import java.util.ArrayList;
 import java.util.List;
-import store.domain.PromotionManager;
-import store.domain.StoreHouse;
 import store.dto.PromotionInfo;
 import store.io.reader.MissionUtilsReader;
 import store.io.reader.Reader;
@@ -14,14 +12,11 @@ public class Config {
     private final Reader reader;
     private final Writer writer;
 
-    private final PromotionManager promotionManager;
-
 
     public Config() {
         this.reader = new MissionUtilsReader();
         this.writer = new SystemWriter();
 
-        this.promotionManager = new PromotionManager(promotionInfos(), new StoreHouse());
     }
 
     public Reader getReader() {
@@ -32,11 +27,7 @@ public class Config {
         return writer;
     }
 
-    public PromotionManager getPromotionManager() {
-        return promotionManager;
-    }
-
-    private List<PromotionInfo> promotionInfos() {
+    private List<PromotionInfo> getPromotionInfos() {
         return new ArrayList<>();
     }
 }
